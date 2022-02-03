92de187a6e5f46bbe0d741ea81b908340b4f89aa
Prassas Antonios <66700258+aprassas@users.noreply.github.com>
2022-02-03 16:33:10 +0200

    Update project.py

--- app/project.py
+++ app/project.py
@@ -3,9 +3,9 @@ import time
 
 def my_function():
     loop=1
-    while(loop <= 9):
+    while(loop <= 2):
         print("hello")
         time.sleep(5.0) 
         loop = loop + 1       
-    #return True
+    return True
 
