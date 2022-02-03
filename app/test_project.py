37a4f3006c43c1c2d07cc2fdd836656e15877b7a
Prassas Antonios <66700258+aprassas@users.noreply.github.com>
2022-02-03 16:36:18 +0200

    Update test_project.py

--- app/test_project.py
+++ app/test_project.py
@@ -3,8 +3,10 @@ import project
 
 print ("running test")
 
-var = project.my_function()
+var = project.my_function2(True)
 assert(var == True)
 
+var = project.my_function2(False)
+assert(var == False)
 print('passed the test')
 
