1ff703c6d52eb931b30e3cc9efc23835accea6dc
aprassas <66700258+aprassas@users.noreply.github.com>
2022-02-03 12:35:46 +0200

    test

--- app/project.py
+++ app/project.py
@@ -1,9 +1,12 @@
 
 import time
 
-loop = 1
-print(loop)
-while(loop <= 9):
-    print("afk")
-    time.sleep(5.0) 
-    loop = loop +1
+def my_function():
+    loop=1
+    while(loop <= 9):
+        print("hello")
+        time.sleep(5.0) 
+        loop = loop + 1       
+    return True
+
+my_function()
---
+++ app/test_project.py
@@ -0,0 +1,8 @@
+import project
+
+print ("running test")
+
+
+project.my_function()
+
+print('passed the test')
